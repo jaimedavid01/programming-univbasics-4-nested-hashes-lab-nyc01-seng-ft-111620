4f11acc0440ba4d2c5502a4bae55e98cd5425404
Jaime David <jaimedavid01@yahoo.com>
2020-10-26 21:16:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,8 @@ def adding_matz
     programmer_hash[:yukihiro_matsumoto] = {}
     programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
       programmer_hash[:yukihiro_matsumoto][:languages] = []
-    programmer_hash[:yukihiro_matsumoto][:languages][1] = "LISP"
-    programmer_hash[:yukihiro_matsumoto][:languages][0] = "C"
+    programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
+    programmer_hash[:yukihiro_matsumoto][:languages][1] = "C"
     
 puts programmer_hash
 
